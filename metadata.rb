abb5ec98b4cc08cfcfb66f910299813c34138bb8
Alexander Merkulov <sasha@merqlove.ru>
2017-09-07 20:46:24 +0300

    Version bump 0.5.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sasha@merqlove.ru'
 license          'Apache 2.0'
 description      'Installs/Configures msmtp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.0'
+version          '0.5.1'
 supports         'debian'
 supports         'ubuntu'
 supports         'centos'
